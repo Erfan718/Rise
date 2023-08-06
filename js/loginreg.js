d515f2669d588f2eb81d9d126cfa140a9d86354a
erfan <khazenierfan@gmail.com>
2023-08-06 23:49:00 +0330

    add change3

--- js/loginreg.js
+++ js/loginreg.js
@@ -1,48 +1,44 @@
-
-var account_btn=  document.getElementById("account-btn");
+var account_btn = document.getElementById("account-btn");
 var login = document.getElementById("login");
 var register = document.getElementById("register");
 
-function Register(){
-  account_btn.style.left="110px";
-  register.style.left="50px";
-  login.style.left="-400px";
+function Register() {
+  account_btn.style.left = "110px";
+  register.style.left = "50px";
+  login.style.left = "-400px";
 }
-function Login(){
-  account_btn.style.right="0px";
-  register.style.left="450px";
-  login.style.left="50px";
+function Login() {
+  account_btn.style.left = "0px";
+  register.style.left = "450px";
+  login.style.left = "50px";
 }
-account_btn.addEventListener("click", function() {
-  Register();
-});
 
-login.addEventListener("click", function() {
-  Login();
-});
+document.getElementById("resume").onchange = function () {
+  document.getElementById("selected-file").innerHTML = this.files[0].name;
+};
 
-document.getElementById("resume").onchange = function() {
-    document.getElementById("selected-file").innerHTML = this.files[0].name;
-  };
-   
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("passwordInput");
 
-  function togglePasswordVisibility() {
-    var passwordInput = document.getElementById("passwordInput");
-    
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
   }
+  var passwordInput = document.getElementById("passwordInput2");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
 
 
-  const fileInput = document.getElementById("resume");
-  const fileLabel = document.getElementById("resumeLabel");
-  
-  fileInput.addEventListener("change", function() {
-    fileLabel.textContent = this.files[0].name;
-  });
 
+const fileInput = document.getElementById("resume");
+const fileLabel = document.getElementById("resumeLabel");
 
-  
+fileInput.addEventListener("change", function () {
+  fileLabel.textContent = this.files[0].name;
+});
